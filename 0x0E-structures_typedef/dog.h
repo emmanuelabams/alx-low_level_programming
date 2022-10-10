089a17345a68fb7c55bad3caf28601fc58d1d93a
emmanuelabams <emmanuelabams23@gmail.com>
2022-10-10 14:50:54 -0700

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,21 @@
-#ifndef STRUCTS
-#define STRUCTS
+#include "dog.h"
+#include <stdlib.h>
 /**
- * struct dog - description for a pet
- * @name: pet name
- * @age: pet age
- * @owner: owner
+ * init_dog - writes the character c to stdout
+ * @d: Dog
+ * @name: Dog name
+ * @age: dog age
+ * @owner: Dog owner
  *
- * Description: Longer description
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-typedef struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-char *name;
-float age;
-char *owner;
-} dog_t;
-#endif
-#ifndef _FUNCTIONS_H
-#define _FUNCTIONS_H
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
+if (d != 0)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
